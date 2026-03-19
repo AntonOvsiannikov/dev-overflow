b523f19a56672d542e8cac4374f955f7ee1c68a3
atrocitus414@gmail.com <deadpool05062001>
2026-03-19 02:27:49 +0200

    do something

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Page = () => {
-  return <div>
-    <h1 className="text-3xl">Welcome to the Home Page</h1>
-  </div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome to the Home Page</h1>
+      <button className="rounded-md bg-blue-500 p-2 text-white">Click me</button>
+    </div>
+  );
 };
 
 export default Page;
